641465f24520aba5d5ebc23da95a373d271d90b8
elazar matan <elazarmata8@gmail.com>
2025-09-02 17:45:45 +0300

    fake leaderboard

--- src/pages/leaderboard.tsx
+++ src/pages/leaderboard.tsx
@@ -1,8 +1,13 @@
 import { Link } from "react-router";
+import loadLeaderboard from "../utils/leaderboard";
 
 export default function Leaderboard(){
+    const myData = loadLeaderboard()
     return <>
     <h1>leaderbord</h1>
+    {myData.map((l:any) => (
+        <p key={l.name}>player: {l.name} time: {l.score}</p>
+    ))}
     <Link to={'/mainPage'}>mainPage</Link>
     </>
 }
