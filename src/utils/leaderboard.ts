684f0efec8c588b2d0c52b2d384a6c67d76aa9bb
elazar matan <elazarmata8@gmail.com>
2025-09-02 17:49:50 +0300

    delete console log

--- src/utils/leaderboard.ts
+++ src/utils/leaderboard.ts
@@ -1,6 +1,5 @@
 export default function loadLeaderboard(){
     const savedata = localStorage.getItem('leader')
-    console.log(savedata)
     const data = savedata? JSON.parse(savedata):null
     return data
 }
