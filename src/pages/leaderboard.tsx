33baf834930a66899b0e3471af5a9cc331d95601
elazar matan <elazarmata8@gmail.com>
2025-09-03 16:52:10 +0300

    Refactor: Load leaderboard data using useEffect and useState
    This commit refactors the Leaderboard component to load leaderboard data using `useEffect` and `useState`. This allows the component to re-render when the data is loaded, ensuring that the leaderboard is displayed correctly.

--- src/pages/leaderboard.tsx
+++ src/pages/leaderboard.tsx
@@ -1,11 +1,15 @@
 import { Link } from "react-router";
 import loadLeaderboard from "../utils/leaderboard";
+import { useEffect, useState } from "react";
 
-export default function Leaderboard(){
-    const myData = loadLeaderboard()
+export default  function Leaderboard(){
+    const [myData, setData] = useState<Array<{ name: string; time: number }>>([]);
+    useEffect(() => {
+        loadLeaderboard(setData)
+    })
     return <>
     <h1>leaderbord</h1>
-    {myData?(myData.map((l:any) => (
+    {myData?(myData.filter((l:any) => l.time).sort((a, b) => a.time - b.time).map((l:any) => (
         <p key={l.name}>player: {l.name} time: {l.time}</p>
     ))):<p>No data found in leaderboard</p>}
     <Link to={'/mainPage'}>mainPage</Link>
