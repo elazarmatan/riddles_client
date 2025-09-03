fc0bc7982fa72383c8c7033ca0de17fdbd71a610
elazar matan <elazarmata8@gmail.com>
2025-09-03 12:11:13 +0300

    ``` feat: Add basic input fields to CreateRiddle page
    This commit adds several text input fields to the CreateRiddle page. These fields are placeholders for riddle data such as name and difficulty.
    ```

--- src/pages/createRiddle.tsx
+++ src/pages/createRiddle.tsx
@@ -3,6 +3,13 @@ import { Link } from "react-router";
 export default function CreateRiddle(){
     return <>
     <h1>create</h1>
+    <input type="text" placeholder="name"/>
+    <input type="text" placeholder="difficulty"/>
+    <input type="text" placeholder=""/>
+    <input type="text" placeholder=""/>
+    <input type="text" placeholder=""/>
+    <input type="text" placeholder=""/>
+    
     <Link to={'/crud'}>crud</Link>
     </>
 }
