35f4edd0972a255b1123ca84f49ec94bc61094db
elazar matan <elazarmata8@gmail.com>
2025-09-03 12:11:46 +0300

    feat: Add props to welcome page and pass data to login
    This commit adds a props to welcome page and pass data to login page.

--- src/pages/welcome.tsx
+++ src/pages/welcome.tsx
@@ -1,13 +1,15 @@
 import { useNavigate } from "react-router";
 
-export default function Welcome(){
+export default function Welcome(props:{setLogin:Function}){
     const navigate = useNavigate()
     return <>
     <h1>welcome</h1>
     <button onClick={()=> {
+        props.setLogin('exist')
         navigate('/login')
     }}>user Exist</button>
      <button onClick={()=> {
+         props.setLogin('new')
         navigate('/login')
     }}>new user</button>
      <button onClick={()=> {
