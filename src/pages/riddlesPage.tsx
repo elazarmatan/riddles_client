acd0d4ece2adcc3f827a3b3a0638f7a46ba0e5cf
elazar matan <elazarmata8@gmail.com>
2025-09-04 15:37:11 +0300

    feat: Fetch and display riddles by level.
    This commit introduces the following changes:
    
    - Fetches riddles based on the level passed as state from the previous page.
    - Uses `getRiddlesById` to fetch riddles from the API.
    - Displays the task description and name of a specific riddle.
    - Adds an input field for the user to enter their answer.
    - Includes a submit button that navigates to the finish page.
    - Displays a "not riddles to load" message if no riddles are fetched.

--- src/pages/riddlesPage.tsx
+++ src/pages/riddlesPage.tsx
@@ -1,20 +1,42 @@
-import { useNavigate } from "react-router";
-import getRiddles from "../utils/getriddles";
+import { useLocation, useNavigate } from "react-router";
+import { useState,useEffect } from "react";
+import getRiddlesById from "../utils/crud/getRiddlesByLevel";
 
-export default function RiddlesPage(){
-    const riddles = getRiddles()
-    const navigate = useNavigate()
-    return <>
-    <h1>show riddles</h1>
-    {riddles ? (
+export default function RiddlesPage() {
+    const location = useLocation()
+    const level = location.state?.level
+  const [dataRiddles, setRiddles] = useState<
+    Array<{
+      name: string;
+      taskDescription: string;
+      hint: string;
+      difficulty: string;
+      riddle_id: number;
+    }>
+  >([]);
+
+  useEffect(() => {
+      getRiddlesById(setRiddles,level)
+      },[])
+  const navigate = useNavigate();
+  return (
+    <>
+      {dataRiddles.length > 0 ? (
         <>
-            <p>{riddles[0].taskDescription}</p>
-            <p>{riddles[0].name}</p>
-            <input type="text" placeholder="answer" />
-            <button onClick={() => {
-                navigate('/finish')
-            }}>submit</button>
+          <p>{dataRiddles[2].taskDescription}</p>
+          <p>{dataRiddles[2].name}</p>
+          <input type="text" placeholder="answer" />
+          <button
+            onClick={() => {
+              navigate("/finish");
+            }}
+          >
+            submit
+          </button>
         </>
-    ) : <p>not riddles to load</p>}
+      ) : (
+        <p>not riddles to load</p>
+      )}
     </>
+  );
 }
