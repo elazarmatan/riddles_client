7e5c432011ba00dd4515cd02e8b42336ba2263a4
elazar matan <elazarmata8@gmail.com>
2025-09-04 15:36:40 +0300

    ``` feat: Implement riddle creation form
    This commit introduces a form for creating new riddles. It includes input fields for name, task description, correct answer, hint, time limit, and difficulty.  A submit button triggers the `createRiddle` function with the form data, and logs the result.
    ```

--- src/pages/createRiddle.tsx
+++ src/pages/createRiddle.tsx
@@ -1,15 +1,48 @@
+import { useEffect, useRef } from "react";
 import { Link } from "react-router";
+import createRiddle from "../utils/crud/createRiddle";
 
-export default function CreateRiddle(){
-    return <>
-    <h1>create</h1>
-    <input type="text" placeholder="name"/>
-    <input type="text" placeholder="difficulty"/>
-    <input type="text" placeholder=""/>
-    <input type="text" placeholder=""/>
-    <input type="text" placeholder=""/>
-    <input type="text" placeholder=""/>
-    
-    <Link to={'/crud'}>crud</Link>
+export default function CreateRiddle() {
+  const name = useRef<HTMLInputElement>(null);
+  const taskDescription = useRef<HTMLInputElement>(null);
+  const correctAnswer = useRef<HTMLInputElement>(null);
+  const hint = useRef<HTMLInputElement>(null);
+  const timeLimit = useRef<HTMLInputElement>(null);
+  const difficulty = useRef<HTMLInputElement>(null);
+  return (
+    <>
+      <h1>create</h1>
+      <form action="">
+        <input type="text" placeholder="name" ref={name} />
+        <input
+          type="text"
+          placeholder="taskDescription"
+          ref={taskDescription}
+        />
+        <input type="text" placeholder="correctAnswer" ref={correctAnswer} />
+        <input type="text" placeholder="hint" ref={hint} />
+        <input type="text" placeholder="timeLimit" ref={timeLimit} />
+        <input type="text" placeholder="difficulty" ref={difficulty} />
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            const riddle = {
+              name: name.current?.value,
+              taskDescription: taskDescription.current?.value,
+              correctAnswer: correctAnswer.current?.value,
+              hint: hint.current?.value,
+              timeLimit: timeLimit.current?.value,
+              difficulty: difficulty.current?.value,
+            };
+            
+              const answer = createRiddle(riddle);
+              console.log(answer);
+          }}
+        >
+          submit
+        </button>
+      </form>
+      <Link to={"/crud"}>crud</Link>
     </>
+  );
 }
