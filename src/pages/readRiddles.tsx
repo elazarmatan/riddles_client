ae1771b2dfb8ed5f7090cf50694be2420c6142d1
elazar matan <elazarmata8@gmail.com>
2025-09-04 15:37:02 +0300

    ``` feat: Display riddles from database
    This commit fetches riddles from the database using `getAllRiddles` and displays them in a list format on the `readRiddles` page.  It includes the riddle ID, name, difficulty, task description, and hint for each riddle.
    ```

--- src/pages/readRiddles.tsx
+++ src/pages/readRiddles.tsx
@@ -1,8 +1,24 @@
 import { Link } from "react-router";
+import getAllRiddles from "../utils/crud/getAllRiddles";
+import { useEffect, useState } from "react";
 
 export default function ReadRiddles(){
+    const [riddles,setRiddles] = useState<Array<{ name: string; taskDescription:string; hint:string; difficulty:string; riddle_id:number}>>([]);
+    useEffect(() => {
+    getAllRiddles(setRiddles)
+    },[])
     return <>
     <h1>read</h1>
     <Link to={'/crud'}>crud</Link>
+    {riddles.map(riddle => (
+        <ul key={riddle.riddle_id}>
+            <li>{riddle.riddle_id}</li>
+            <li>{riddle.name}</li>
+            <li>{riddle.difficulty}</li>
+            <li>{riddle.taskDescription}</li>
+            <li>{riddle.hint}</li>
+            <li></li>
+        </ul>
+    ))}
     </>
 }
