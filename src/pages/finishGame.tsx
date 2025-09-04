c21b99b1ede8ce8ba4c51a505f5494b35f9683e8
elazar matan <elazarmata8@gmail.com>
2025-09-04 17:11:32 +0300

    feat: Display game time on finish page
    This commit adds the display of the game time on the finish page, passing the time via `location.state`.

--- src/pages/finishGame.tsx
+++ src/pages/finishGame.tsx
@@ -1,9 +1,10 @@
-import { Link } from "react-router";
+import { Link, useLocation } from "react-router";
 
 export default function FinishGame(){
+    const location = useLocation()
     return <>
     <h1>record</h1>
-    <p>your own record 8.32 seconds</p>
+    <p>your own record {location.state?.seconds} seconds</p>
     <Link to={'/mainPage'}>mainPage</Link>
     </>
 }
